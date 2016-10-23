08f3e0aba836a4c41c0eefa882a4944d2f203184
katerinassem <katarina.sem@mail.ru>
2016-10-23 20:26:36 +0300

    SH-9 - Login controller and service (#4)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,19 +7,19 @@ angular
   .module('shokenWebApp', [
     'ngRoute'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/root/root.html',
-        controller: 'RootCtrl',
-        controllerAs: 'root'
-      })
-      .when('/login', {
-        templateUrl: 'views/login/login.html',
-        controller: 'LoginCtrl',
-        controllerAs: 'login'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+          .when('/', {
+            templateUrl: 'views/root/root.html',
+            controller: 'RootCtrl',
+            controllerAs: 'root'
+          })
+          .when('/login', {
+            templateUrl: 'views/login/login.html',
+            controller: 'LoginCtrl',
+            controllerAs: 'login'
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+      }]);
