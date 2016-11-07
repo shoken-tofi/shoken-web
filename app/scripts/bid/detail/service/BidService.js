15388f9d04ad8c10bfa0232361470f2abb19f692
katerinassem <katarina.sem@mail.ru>
2016-11-07 17:24:50 +0300

    Bid (#9)
    * Fixed bid page
    
    * Bid page
    
    * Bid page complete ui

--- app/scripts/bid/detail/service/BidService.js
+++ app/scripts/bid/detail/service/BidService.js
@@ -18,6 +18,7 @@ angular.module('shokenWebApp')
             "description": "High profit bid of type bond.",
             "quantity": 1,
             "startPrice": 11.20,
+            "currentBet": 14.55,
             "startDate": "2016-10-28 19:00:00",
             "endDate": "2016-10-30 19:00:00",
             "comment": "It's a great bid that will bring you a lot of luck! Click to bet!",
@@ -28,7 +29,29 @@ angular.module('shokenWebApp')
               "id": "1",
               "fullname": "Alex Brown",
               "rate": 73
-            }
+            },
+            "investors": [
+              {
+                "name": "carmen lol11",
+                "betDate": "2016-10-28 22:03:05",
+                "bet": 14.55
+              },
+              {
+                "name": "juicy zzz",
+                "betDate": "2016-10-28 22:00:56",
+                "bet": 14.00
+              },
+              {
+                "name": "quick fox aa8",
+                "betDate": "2016-10-28 21:03:01",
+                "bet": 12.00
+              },
+              {
+                "name": "random apple 22",
+                "betDate": "2016-10-28 19:55:55",
+                "bet": 11.99
+              }
+            ]
           };
 
           successCallback(bid);
