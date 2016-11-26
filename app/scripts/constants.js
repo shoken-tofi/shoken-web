42784309e0f7e596f6e2e9878917752ca3634f49
katerinassem <katarina.sem@mail.ru>
2016-11-26 22:44:33 +0300

    Enhanced contsants

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -3,8 +3,12 @@
 /*
  * Constants for the shokenWebApp
  */
+
+var backendUrl = "http://shoken-back.herokuapp.com";
+
 angular
   .module('shokenWebApp')
+  .constant('BACKEND_URL', backendUrl)
   .constant('API', {
-    'url': 'http://localhost:8080'
+    'bids': backendUrl + '/bids'
   });
