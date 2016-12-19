93104d9a29b124d2c3cbcfb473fa30e2ea7f39c5
katerinassem <katarina.sem@mail.ru>
2016-12-20 01:00:34 +0300

    Fixed filter

--- app/scripts/bid/list/service/BidsListService.js
+++ app/scripts/bid/list/service/BidsListService.js
@@ -26,23 +26,17 @@ angular.module('shokenWebApp')
             params.bidTypes.push(params.type);
           }
 
-          params.minStartDateHoursGone = params.recent ? 0 : -1;
-          params.maxStartDateHoursGone = params.recent ? 24 : -1;
+          params.minStartDateHoursGone = params.recent ? 0 : undefined;
+          params.maxStartDateHoursGone = params.recent ? 24 : undefined;
 
           params.minStartDateHoursGone = params.progress ? params.recent ? params.minStartDateHoursGone : 24 : params.minStartDateHoursGone;
-          params.maxStartDateHoursGone = params.progress ? -1 : params.maxStartDateHoursGone;
+          params.maxStartDateHoursGone = params.progress ? undefined : params.maxStartDateHoursGone;
 
-          params.minExpirationDateHoursLeft = params.expiring ? 0 : -1;
-          params.maxExpirationDateHoursLeft = params.expiring ? 24 : -1;
+          params.minExpirationDateHoursLeft = params.expiring ? 0 : undefined;
+          params.maxExpirationDateHoursLeft = params.expiring ? 24 : undefined;
 
           params.minExpirationDateHoursLeft = params.progress ? params.expiring ? params.minExpirationDateHoursLeft : 24 : params.minExpirationDateHoursLeft;
-          params.maxExpirationDateHoursLeft = params.progress ? -1 : params.maxExpirationDateHoursLeft;
-
-          // params.minStartDateHoursGone = params.progress ? 24 : -1;
-          // params.maxStartDateHoursGone = params.recent ? 24 : -1;
-          //
-          // params.minExpirationDateHoursLeft = params.progress ? 24 : -1;
-          // params.maxExpirationDateHoursLeft = params.expiring ? 24 : -1;
+          params.maxExpirationDateHoursLeft = params.progress ? undefined : params.maxExpirationDateHoursLeft;
 
           console.log(params);
 
